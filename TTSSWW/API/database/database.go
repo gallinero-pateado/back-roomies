e949d84d06826da1dab462db17a4ee9d1e7af605
sllach <sllach@utem.cl>
2024-11-14 22:52:30 -0300

    Integración pool de conexiones y configuración CORS

--- TTSSWW/API/database/database.go
+++ TTSSWW/API/database/database.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"backend/API/config"
+	"log"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,5 +14,20 @@ func OpenGormDB() (*gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Obtener la conexión subyacente *sql.DB para configurar el pool de conexiones
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	// Configurar el pool de conexiones
+	sqlDB.SetMaxOpenConns(25)                 // Número máximo de conexiones abiertas
+	sqlDB.SetMaxIdleConns(25)                 // Número máximo de conexiones inactivas
+	sqlDB.SetConnMaxLifetime(5 * time.Minute) // Tiempo máximo de vida de una conexión
+
+	// Puedes agregar logs para verificar que se ha configurado correctamente
+	log.Println("Pool de conexiones configurado correctamente")
+
 	return db, nil
 }
