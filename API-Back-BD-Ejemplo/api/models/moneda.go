838f712cc3d33a0c28b455a012f20c2936a6c1e1
sllach <sllach@utem.cl>
2024-11-19 00:29:51 -0300

    Delete API-Back-BD-Ejemplo directory

--- API-Back-BD-Ejemplo/api/models/moneda.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Moneda struct {
-	ID         uint   `json:"id" gorm:"primaryKey"`
-	Nombre     string `json:"nombre"`
-	Cantidad   int    `json:"cantidad"`
-	MonederoID uint   `json:"monederoID"` // Clave foránea a Monedero
-}
